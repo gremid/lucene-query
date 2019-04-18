2304a090b27fb7037ca5b8302526a4a27496569e
Gregor Middell <gregor@middell.net>
2019-04-18 14:23:44 +0200

    Minor formatting

--- test/lucene_query/core_test.clj
+++ test/lucene_query/core_test.clj
@@ -1,18 +1,18 @@
 (ns lucene-query.core-test
   (:require [clojure.test :refer :all]
-            [lucene-query.core :as lq]
+            [lucene-query.core :as lucene]
             [midje.sweet :as midje]))
 
 (deftest roundtrip
-  (let [str->ast->str (comp lq/ast->str lq/str->ast)
-        rt (fn [q] (midje/fact (str->ast->str q) => q))]
+  (let [str->ast->str (comp lucene/ast->str lucene/str->ast)
+        roundtrip #(midje/fact (str->ast->str %) => %)]
     (midje/facts
-     (rt "-*:test OR +test2")
-     (rt "!test OR test3")
-     (rt "!test OR [* TO 2019-01-01]")
-     (rt "/te\\/st/")
-     (rt "\"test \\\" 1\"~2")
-     (rt "autor:me^10 OR def:\"*gu\\*t*\"")
-     (rt "autor:test AND form:*te")
-     (rt "status:(Red-f OR Red-2 OR Artikelrump*)")
-     (rt "status:(Red-f OR Red-2 OR Artikelrump*) AND !autor:me"))))
+     (roundtrip "-*:test OR +test2")
+     (roundtrip "!test OR test3")
+     (roundtrip "!test OR [* TO 2019-01-01]")
+     (roundtrip "/te\\/st/")
+     (roundtrip "\"test \\\" 1\"~2")
+     (roundtrip "autor:me^10 OR def:\"*gu\\*t*\"")
+     (roundtrip "autor:test AND form:*te")
+     (roundtrip "status:(Red-f OR Red-2 OR Artikelrump*)")
+     (roundtrip "status:(Red-f OR Red-2 OR Artikelrump*) AND !autor:me"))))
