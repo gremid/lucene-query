c618c894fe287ed61aaac192824cd2c2ac05c1dc
Gregor Middell <gregor@middell.net>
2019-04-18 09:42:05 +0200

    Initial checkin

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject gremid/lucene-query "0.1.0-SNAPSHOT"
+  :description "Parser for Apache Lucene Classic Queries"
+
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [instaparse "1.4.10"]]
+  :repl-options {:init-ns lucene-query.core})
---
+++ src/lucene_query/core.clj
@@ -0,0 +1,19 @@
+(ns lucene-query.core
+  (:require [clojure.java.io :as io]
+            [instaparse.core :as insta :refer [defparser]]))
+
+(defparser query-parser
+  (io/resource "query.bnf"))
+
+(defn strcat [type]
+  (fn [& args]
+    (if (every? string? args)
+      [type (apply str args)]
+      (vec (concat [type] args)))))
+
+(def str-nodes [:field :value :regexp :quoted :fuzzy :boost :bound])
+(def transforms (into {:all (constantly :all)}
+                      (map #(vector % (strcat %)) str-nodes)))
+(def parse (comp (partial insta/transform transforms) query-parser))
+
+;;(time (parse "(-text_ss:/test/^1.0 OR *:[2019-10-01 TO *} OR autor:(neu OR \"alt\\\"\"))^3.0 AND +filter:true~10"))
